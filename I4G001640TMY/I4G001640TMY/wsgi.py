613a40b698015bb12fb8942f3c9077e740e8e87c
Ogirimah <93133254+Ogirimah@users.noreply.github.com>
2022-06-19 13:35:22 +0100

    Changed the project name from Ogirimah to I4G001640TMY

--- I4G001640TMY/I4G001640TMY/wsgi.py
+++ I4G001640TMY/I4G001640TMY/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Ogirimah project.
+WSGI config for I4G001640TMY project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Ogirimah.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'I4G001640TMY.settings')
 
 application = get_wsgi_application()
--- I4G001640TMY/manage.py
+++ I4G001640TMY/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Ogirimah.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'I4G001640TMY.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
